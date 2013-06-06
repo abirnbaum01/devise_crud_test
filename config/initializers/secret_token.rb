d19c96ec7cec79534316376a5b01e7ec0f3225d4
Adam Birnbaum <birnbaum.adam@gmail.com>
2013-06-05 22:17:16 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseCrudTest::Application.config.secret_token = '6b8c0b74394ed0e4d1acbae9d30e3e2e93e146bdc161f0b7f2107913b8c5653c7146268c6f76d3aeff13d3ce8e248e45b44a96759121d3fef6ade92a03dba456'
